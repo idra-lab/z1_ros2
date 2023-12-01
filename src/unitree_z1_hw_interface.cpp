3eeec56e6f089bdd6957c7961af65ee3e8807edd
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-12-01 15:10:52 +0100

    Improved documentation of functions

--- src/unitree_z1_hw_interface.cpp
+++ src/unitree_z1_hw_interface.cpp
@@ -1,11 +1,26 @@
+#include <stdexcept>
+#include <string>
+#include <cctype>
+#include <unitree_arm_sdk/control/unitreeArm.h>
 #include <vector>
 
 #include <hardware_interface/hardware_info.hpp>
 #include <hardware_interface/types/hardware_interface_return_values.hpp>
+#include <rclcpp/logging.hpp>
 #include <unitree_z1_hw_interface/unitree_z1_hw_interface.hpp>
-#include <unitree_arm_sdk/control/unitreeArm.h>
+#include "hardware_interface/system_interface.hpp"
 
 using unitree::z1::hw_interface::UnitreeZ1HWInterface;
+
+
+//  ____            _                 _   _
+// |  _ \  ___  ___| | __ _ _ __ __ _| |_(_) ___  _ __  ___
+// | | | |/ _ \/ __| |/ _` | '__/ _` | __| |/ _ \| '_ \/ __|
+// | |_| |  __/ (__| | (_| | | | (_| | |_| | (_) | | | \__ \
+// |____/ \___|\___|_|\__,_|_|  \__,_|\__|_|\___/|_| |_|___/
+//
+
+static void to_lower_string(const std::string& str);
 
 //   ____                _                   _
 //  / ___|___  _ __  ___| |_ _ __ _   _  ___| |_ ___  _ __ ___
@@ -40,31 +55,50 @@ UnitreeZ1HWInterface::~UnitreeZ1HWInterface() {
  *
  */
 
+/**
+ * This function should setup the communication with the hardware and activate it.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_configure(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
 }
 
+/**
+ * This function should clean up the communication with the hardware and deactivate it.
+ * Opposite of on_configure.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_cleanup(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
 }
 
+/**
+ * This function should shutdown the hardware.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_shutdown(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
 }
 
+/**
+ * This function should activate the hardware.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_activate(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
 }
 
+/**
+ * This function should deactivate the hardware.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_deactivate(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
 }
 
+/**
+ * This function should handle errors.
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_error(
         const rclcpp_lifecycle::State& prev_state) {
     // TODO
@@ -77,6 +111,10 @@ hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_error(
 // |_| |_|  \_/\_/    |___|_| |_|\__\___|_|  |_|  \__,_|\___\___|
 //
 
+/**
+ * This function is responsible to initialize parameters coming from the URDF file
+ * through "hw_info".
+ */
 hardware_interface::CallbackReturn UnitreeZ1HWInterface::on_init(
         const hardware_interface::HardwareInfo& hw_info) {
     // TODO
@@ -108,3 +146,23 @@ hardware_interface::return_type UnitreeZ1HWInterface::write(
 // |  __/| |  | |\ V / (_| | ||  __/
 // |_|   |_|  |_| \_/ \__,_|\__\___|
 //
+
+
+
+//  ____  _        _   _
+// / ___|| |_ __ _| |_(_) ___ ___
+// \___ \| __/ _` | __| |/ __/ __|
+//  ___) | || (_| | |_| | (__\__ \
+// |____/ \__\__,_|\__|_|\___|___/
+//
+
+/**
+ * @bried Convert a string to lower case.
+ *
+ * @param str       The string to be converted.
+ */
+static void to_lower_string(const std::string& str) {
+    std::transform(str.begin(), str.end(), str.begin(),
+                          [](unsigned char c) { return std::tolower(c); }
+    );
+}
