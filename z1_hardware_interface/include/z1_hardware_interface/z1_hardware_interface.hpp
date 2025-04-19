ac30d09e49b320a06761cd9043db4eafd3ded680
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2025-04-19 13:41:51 +0000

    Hardware interface implementation widh SDK communication

--- z1_hardware_interface/include/z1_hardware_interface/z1_hardware_interface.hpp
+++ z1_hardware_interface/include/z1_hardware_interface/z1_hardware_interface.hpp
@@ -10,12 +10,14 @@
 #include "rclcpp/logger.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "unitree_arm_sdk/control/unitreeArm.h"
 
 namespace unitree::z1 {
 
 class HardwareInterface : public hardware_interface::SystemInterface {
 public:
-    using Vec6 = Eigen::Vector<double, 6>;  // NOLINT: magic number
+    using Vec6   = Eigen::Vector<double, 6>;  // NOLINT: magic number
+    using ArmPtr = std::unique_ptr<UNITREE_ARM::unitreeArm>;
 
     RCLCPP_SHARED_PTR_DEFINITIONS(HardwareInterface)
 
@@ -52,10 +54,6 @@ public:
             const rclcpp_lifecycle::State& prev_state
     ) override;
 
-    hardware_interface::CallbackReturn on_init(
-            const hardware_interface::HardwareInfo& hw_info
-    ) override;
-
     std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
     std::vector<hardware_interface::CommandInterface>
@@ -75,14 +73,59 @@ public:
             const std::vector<std::string>& stop_interfaces
     ) override;
 
+    // clang-format off
+    [[nodiscard]] std::vector<hardware_interface::ComponentInfo>& joints() { return info_.joints; }
+    [[nodiscard]] const std::vector<hardware_interface::ComponentInfo>& joints() const { return info_.joints; }
+
+    [[nodiscard]] rclcpp::Logger& get_logger() { return _logger; }
+    
+    [[nodiscard]] bool with_gripper() const;
+    // clang-format on
+
 
 private:
     rclcpp::Logger _logger = rclcpp::get_logger("z1_hardware_interface");
 
-    rclcpp::Logger&
-    get_logger() {
-        return _logger;
-    }
+    ArmPtr _arm = nullptr;
+
+    Vec6   _arm_max_torque     = 20.0 * Vec6::Ones();
+    double _gripper_max_torque = 20.0;
+
+    struct GainsData {
+        std::vector<double> kp = {20.0, 30.0, 30.0, 20.0, 15.0, 10.0, 20.0};
+        std::vector<double> kd = {2000, 2000, 2000, 2000, 2000, 2000, 2000};
+    };
+
+    GainsData _default_gains;
+    GainsData _current_gains;
+
+    struct {
+        Vec6 q   = Vec6::Zero();
+        Vec6 qd  = Vec6::Zero();
+        Vec6 tau = Vec6::Zero();
+    } _arm_state;
+
+    struct {
+        double q   = 0;
+        double qd  = 0;
+        double tau = 0;
+    } _gripper_state;
+
+    struct {
+        Vec6 q   = Vec6::Zero();
+        Vec6 qd  = Vec6::Zero();
+        Vec6 tau = Vec6::Zero();
+    } _arm_cmd;
+
+    struct {
+        double q   = 0;
+        double qd  = 0;
+        double tau = 0;
+    } _gripper_cmd;
+
+    void saturate_torque();
+
+    long get_joint_id(const std::string& joint_name) const;
 };
 
 
--- z1_hardware_interface/src/z1_hardware_interface.cpp
+++ z1_hardware_interface/src/z1_hardware_interface.cpp
@@ -1,5 +1,16 @@
 #include "z1_hardware_interface/z1_hardware_interface.hpp"
 
+#include <algorithm>
+#include <ctime>
+#include <fmt/format.h>
+#include <memory>
+#include <stdexcept>
+#include <unitree_arm_sdk/control/unitreeArm.h>
+#include <unitree_arm_sdk/message/arm_common.h>
+
+#include <rclcpp/duration.hpp>
+
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/logging.hpp"
 
 //  ____            _                 _   _
@@ -10,6 +21,16 @@
 //
 
 using unitree::z1::HardwareInterface;
+
+static void to_lower_string(std::string& str);
+
+template <typename Iterable>
+static std::string
+pretty_vector(const Iterable& vec) {
+    return fmt::format("({})", fmt::join(vec, ", "));
+}
+
+static std::pair<std::string, std::string> split_interface(const std::string&);
 
 //  ____   ____ _     ____ ____  ____    _     _  __       ____           _
 // |  _ \ / ___| |   / ___|  _ \|  _ \  | |   (_)/ _| ___ / ___|   _  ___| | ___
@@ -25,7 +46,49 @@ HardwareInterface::on_configure(const rclcpp_lifecycle::State& prev_state) {
         RCLCPP_ERROR(get_logger(), "parent on_configure() failed");
         return hardware_interface::CallbackReturn::ERROR;
     }
-    // TODO
+
+#ifdef SHOW_DEBUG_MESSAGES
+    rclcpp::Logger logger = get_logger();
+    logger.set_level(rclcpp::Logger::Level::Debug);
+#endif
+
+
+    if (with_gripper()) RCLCPP_INFO(get_logger(), "Gripper is enabled");
+    else RCLCPP_INFO(get_logger(), "Gripper is disabled");
+
+    // TODO: load torque limits from URDF
+    RCLCPP_INFO(
+            get_logger(),
+            "Joint torque limits: %s",
+            pretty_vector(_arm_max_torque).c_str()
+    );
+    RCLCPP_INFO(get_logger(), "Gripper torque limit: %lf", _gripper_max_torque);
+
+    RCLCPP_INFO(get_logger(), "Establishing connection to the ARM through SDK");
+    _arm = std::make_unique<UNITREE_ARM::unitreeArm>(with_gripper());
+    RCLCPP_INFO(get_logger(), "Connection established!");
+    _arm->sendRecvThread->start();
+    _arm->setFsm(UNITREE_ARM::ArmFSMState::PASSIVE);
+    read(rclcpp::Time(0), rclcpp::Duration(0, 0));
+
+    // clang-format off
+    RCLCPP_INFO(get_logger(), "Current joints configuration: %s", pretty_vector(_arm_state.q).c_str());
+    RCLCPP_INFO(get_logger(), "Current joints velocity: %s", pretty_vector(_arm_state.qd).c_str());
+    RCLCPP_INFO(get_logger(), "Measured joint torque: %s", pretty_vector(_arm_state.tau).c_str());
+    RCLCPP_INFO(get_logger(), "Position-proportional gains: %s", pretty_vector(_default_gains.kp).c_str());
+    RCLCPP_INFO(get_logger(), "Velocity-proportional gains: %s", pretty_vector(_default_gains.kd).c_str());
+    // clang-format on
+
+
+    // Set command to current state
+    _arm_cmd.q      = _arm_state.q;
+    _arm_cmd.qd     = _arm_state.qd;
+    _gripper_cmd.q  = _gripper_state.q;
+    _gripper_cmd.qd = _gripper_state.qd;
+    _arm->setArmCmd(_arm_state.q, _arm_state.qd);
+    _arm->setFsm(UNITREE_ARM::ArmFSMState::LOWCMD);
+    RCLCPP_INFO(get_logger(), "SDK switch to low-level control!");
+
     RCLCPP_DEBUG(get_logger(), "on_configure() completed successfully");
     return hardware_interface::CallbackReturn::SUCCESS;
 }
@@ -50,7 +113,8 @@ HardwareInterface::on_shutdown(const rclcpp_lifecycle::State& prev_state) {
         != hardware_interface::CallbackReturn::SUCCESS) {
         RCLCPP_ERROR(get_logger(), "parent on_shutdown() failed");
     }
-    // TODO
+    RCLCPP_INFO(get_logger(), "Closing SDK connection");
+    _arm->sendRecvThread->shutdown();
     RCLCPP_DEBUG(get_logger(), "on_shutdown() completed successfully");
     return hardware_interface::CallbackReturn::SUCCESS;
 }
@@ -106,47 +170,76 @@ HardwareInterface::on_error(const rclcpp_lifecycle::State& prev_state) {
 // |_| |_|  \_/\_/    |___|_| |_|\__\___|_|  |_|  \__,_|\___\___|
 //
 
-hardware_interface::CallbackReturn
-HardwareInterface::on_init(const hardware_interface::HardwareInfo& hw_info) {
-#ifdef SHOW_DEBUG_MESSAGES
-    rclcpp::Logger logger = get_logger();
-    logger.set_level(rclcpp::Logger::Level::Debug);
-#endif
-
-    RCLCPP_DEBUG(get_logger(), "calling on_init()");
-    if (hardware_interface::SystemInterface::on_init(hw_info)
-        != hardware_interface::CallbackReturn::SUCCESS) {
-        RCLCPP_ERROR(get_logger(), "parent on_init() failed");
-        return hardware_interface::CallbackReturn::ERROR;
-    }
-    RCLCPP_DEBUG(get_logger(), "on_init() completed successfully");
-    // TODO
-    return hardware_interface::CallbackReturn::SUCCESS;
-}
-
 std::vector<hardware_interface::StateInterface>
 HardwareInterface::export_state_interfaces() {
+    using hardware_interface::HW_IF_EFFORT;
+    using hardware_interface::HW_IF_POSITION;
+    using hardware_interface::HW_IF_VELOCITY;
+
     std::vector<hardware_interface::StateInterface> state_interfaces;
-    // TODO
+    state_interfaces.reserve(21);  // NOLINT: 7 joints * 3 states
+    for (long i = 0; i < 6; ++i) {
+        const std::string jnt_name = joints()[i].name;
+        state_interfaces.emplace_back(jnt_name, HW_IF_POSITION, &_arm_state.q(i));
+        state_interfaces.emplace_back(jnt_name, HW_IF_VELOCITY, &_arm_state.qd(i));
+        state_interfaces.emplace_back(jnt_name, HW_IF_EFFORT, &_arm_state.tau(i));
+    }
+    if (with_gripper()) {
+        const std::string jnt_name = joints()[6].name;
+        state_interfaces.emplace_back(jnt_name, HW_IF_POSITION, &_gripper_state.q);
+        state_interfaces.emplace_back(jnt_name, HW_IF_VELOCITY, &_gripper_state.qd);
+        state_interfaces.emplace_back(jnt_name, HW_IF_EFFORT, &_gripper_state.tau);
+    }
     return state_interfaces;
 };
 
 std::vector<hardware_interface::CommandInterface>
 HardwareInterface::export_command_interfaces() {
-    std::vector<hardware_interface::CommandInterface> command_interfaces;
-    // TODO
-    return command_interfaces;
+    using hardware_interface::HW_IF_EFFORT;
+    using hardware_interface::HW_IF_POSITION;
+    using hardware_interface::HW_IF_VELOCITY;
+
+    std::vector<hardware_interface::CommandInterface> cmd_interfaces;
+    cmd_interfaces.reserve(21);  // NOLINT: 7 joints * 3 cmd interfaces
+    for (long i = 0; i < 6; ++i) {
+        const std::string jnt_name = joints()[i].name;
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_POSITION, &_arm_cmd.q(i));
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_VELOCITY, &_arm_cmd.qd(i));
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_EFFORT, &_arm_cmd.tau(i));
+    }
+    if (with_gripper()) {
+        const std::string jnt_name = joints()[6].name;
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_POSITION, &_gripper_cmd.q);
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_VELOCITY, &_gripper_cmd.qd);
+        cmd_interfaces.emplace_back(jnt_name, HW_IF_EFFORT, &_gripper_cmd.tau);
+    }
+    return cmd_interfaces;
 }
 
 hardware_interface::return_type
 HardwareInterface::
         read(const rclcpp::Time& /* time */, const rclcpp::Duration& /* period */) {
+    _arm->sendRecv();
+    for (long i = 0; i < 6; ++i) {
+        _arm_state.q(i)   = _arm->lowstate->q[i];
+        _arm_state.qd(i)  = _arm->lowstate->dq[i];
+        _arm_state.tau(i) = _arm->lowstate->tau[i];
+    }
+    if (with_gripper()) {
+        _gripper_state.q   = _arm->lowstate->q[6];
+        _gripper_state.qd  = _arm->lowstate->dq[6];
+        _gripper_state.tau = _arm->lowstate->tau[6];
+    }
     return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type
 HardwareInterface::
         write(const rclcpp::Time& /* time */, const rclcpp::Duration& /* period */) {
+    saturate_torque();
+    _arm->setArmCmd(_arm_cmd.q, _arm_cmd.qd, _arm_cmd.tau);
+    _arm->setGripperCmd(_gripper_cmd.q, _gripper_cmd.qd, _gripper_cmd.tau);
+    _arm->sendRecv();
     return hardware_interface::return_type::OK;
 }
 
@@ -155,6 +248,41 @@ HardwareInterface::perform_command_mode_switch(
         const std::vector<std::string>& start_interfaces,
         const std::vector<std::string>& /* stop_interfaces */
 ) {
+    using hardware_interface::HW_IF_EFFORT;
+    using hardware_interface::HW_IF_POSITION;
+    using hardware_interface::HW_IF_VELOCITY;
+
+    RCLCPP_INFO(get_logger(), "Switching control mode");
+    for (const std::string& interface : start_interfaces) {
+        const auto [name, type] = split_interface(interface);
+        auto idx                = get_joint_id(name);
+
+        if (type == HW_IF_POSITION) {
+            _current_gains.kp[idx] = _default_gains.kp[idx];
+            _current_gains.kd[idx] = _default_gains.kd[idx];
+        } else if (type == HW_IF_VELOCITY) {
+            _current_gains.kp[idx] = 0.0;
+            _current_gains.kd[idx] = _default_gains.kd[idx];
+        } else if (type == HW_IF_EFFORT) {
+            _current_gains.kp[idx] = 0.0;
+            _current_gains.kd[idx] = 0.0;
+        } else {
+            RCLCPP_ERROR(
+                    get_logger(),
+                    "Don't know how to configure interface '%s'",
+                    interface.c_str()
+            );
+            return hardware_interface::return_type::ERROR;
+        }
+    }
+
+    // clang-format off
+    RCLCPP_INFO(get_logger(), "Updated proportional gains: %s", pretty_vector(_current_gains.kp).c_str());
+    RCLCPP_INFO(get_logger(), "Updated derivative gains: %s", pretty_vector(_current_gains.kd).c_str());
+    // clang-format on
+
+    _arm->lowcmd->setControlGain(_current_gains.kp, _current_gains.kd);
+    _arm->lowcmd->setGripperGain(_current_gains.kp[6], _current_gains.kd[6]);
     return hardware_interface::return_type::OK;
 }
 
@@ -165,6 +293,33 @@ HardwareInterface::perform_command_mode_switch(
 // |_|   |_|  |_| \_/ \__,_|\__\___|
 //
 
+void
+HardwareInterface::saturate_torque() {
+    const Vec6 original_tau = _arm_cmd.tau;
+    _arm_cmd.tau = original_tau.cwiseMin(_arm_max_torque).cwiseMax(-_arm_max_torque);
+    _gripper_cmd.tau =
+            std::clamp(_gripper_cmd.tau, -_gripper_max_torque, _gripper_max_torque);
+
+    if (original_tau != _arm_cmd.tau)
+        RCLCPP_WARN(get_logger(), "Saturating input torque");
+}
+
+bool
+HardwareInterface::with_gripper() const {
+    std::string gripper_param = info_.hardware_parameters.at("gripper");
+    to_lower_string(gripper_param);
+    return gripper_param == "true";
+}
+
+long
+HardwareInterface::get_joint_id(const std::string& joint_name) const {
+    for (long i = 0; i < joints().size(); ++i) {
+        if (joints()[i].name == joint_name) return i;
+    }
+    throw std::out_of_range(
+            fmt::format("Unable to find joint '{}' with the joints of the robot")
+    );
+}
 
 //  ____  _        _   _
 // / ___|| |_ __ _| |_(_) ___ ___
@@ -183,6 +338,14 @@ to_lower_string(std::string& str) {
     std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {
         return std::tolower(c);
     });
+}
+
+static std::pair<std::string, std::string>
+split_interface(const std::string& in) {
+    const auto sep_id = in.find('/');
+    return std::make_pair(
+            in.substr(0, sep_id), in.substr(sep_id + 1, in.size() - sep_id - 1)
+    );
 }
 
 //  _____                       _
