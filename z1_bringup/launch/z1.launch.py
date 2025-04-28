c843276b872f8810ee1206c5714be05a7d0cd8ae
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2025-04-28 19:37:25 +0000

    Add LICENSE And insertion of banner notice in relevant source files

--- z1_bringup/launch/z1.launch.py
+++ z1_bringup/launch/z1.launch.py
@@ -1,3 +1,18 @@
+# Copyright 2025 IDRA, University of Trento
+# Author: Matteo Dalle Vedove (matteodv99tn@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from launch.event_handlers.on_process_exit import OnProcessExit
 import xacro
 import os
@@ -94,8 +109,7 @@ def launch_setup(context, *args, **kwargs):
         executable="rviz2",
         output="screen",
         arguments=[
-            "-d",
-            os.path.join(get_package_share_path("z1_bringup"), "rviz", "z1.rviz")
+            "-d", os.path.join(get_package_share_path("z1_bringup"), "rviz", "z1.rviz")
         ],
         condition=IfCondition(rviz),
     )
